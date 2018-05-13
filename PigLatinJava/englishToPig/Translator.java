dd1a83f8876958636223275e9679a53b7cc72705
Max Beyer <inflatableanchor@gmail.com>
2018-05-12 21:37:16 -0500

    Code totally works.  Accounts for punctuation, capitalization, and everything else other translators mess up.

--- PigLatinJava/englishToPig/Translator.java
+++ PigLatinJava/englishToPig/Translator.java
@@ -5,37 +5,45 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Translator {
+	
+	//private String[] vowelStringArray = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"};
+	private List<String> vowels = new ArrayList<String>(Arrays.asList(new String[] {"a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y"}));
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Translator translator = new Translator();
+		String test = "And now, for my first trick, I shall perform an act that has never been seen before!  Witness, Terry's Turnaround!";
+		String translation = translator.translateSentence(test);
+		System.out.print(translation);
 
 	}
-
+	
+	//save each word of the string into an array using the word regular expression. This ensures punctuation remains.
+	//loop through the string, replacing each matched word from the array with a pig latin translation
 	public String translateSentence(String stringToTranslate) {
-		// is it possible to use the replace function instead of deconstructing and reconstructing the sentence?
-		//stringToTranslate.replaceAll("regex for words including ' character", thatWordRunThroughTheWordTranslator);
-		String pigSentence = "";
-		List<String> wordsArray = new ArrayList<String>(Arrays.asList(stringToTranslate.split(" ")));
-		int arrayLength = wordsArray.size();
-		for(int i = 0; i < arrayLength - 1; i++) { //spaces out each word up until the last word
-			pigSentence += translateWord(wordsArray.get(i)) + " ";
-			//strip ending punctuation out with regex
-			//bring puntuation to end of word using -1 index
+		Translator translator = new Translator();
+		String translatedString = stringToTranslate;
+		List<String> wordsArray = new ArrayList<String>(Arrays.asList(stringToTranslate.split("[^\\w\'+]")));
+		translatedString = translatedString.replaceFirst(wordsArray.get(0), translator.translateWord(wordsArray.get(0)));
+		for(int i = 1; i < wordsArray.size(); i++) { //iterator starts at 1, because first word has been translated
+			//replacing with a space character before the regex SHOULD be the thing that eliminates the possibility of translating an already translated word
+			translatedString = translatedString.replaceFirst(" " + wordsArray.get(i), " " + translator.translateWord(wordsArray.get(i)));
 		}
-		pigSentence += translateWord(wordsArray.get(-1)); // appends final word without a trailing space
-		return pigSentence;
+		return translatedString;
 	}
 	
 	public String translateWord(String word) {
-		int firstVowelIndex = findFirstVowel(word);
+		if(word.matches("[^\\w+]") || "".equals(word)) {
+			return word;
+		}
 		String pigString = "";
-		if(firstVowelIndex > 0) {
+		int firstVowelIndex = findFirstVowel(word);
+		if(firstVowelIndex > 0) { //word does not begin with vowel
 			pigString = word.substring(firstVowelIndex) + "-" + word.substring(0, firstVowelIndex) + "ay";
 			if(!pigString.equals(pigString.toLowerCase())) {
 				pigString = pigString.toLowerCase();
 				pigString = pigString.substring(0, 1).toUpperCase() + pigString.substring(1);
 			}
-		} else {
+		} else { //word begins with vowel
 			pigString = word + "-yay";
 		}
 		return pigString;
@@ -44,40 +52,14 @@ public class Translator {
 	//if word has a vowel, return value will be greater than -1
 	public Integer findFirstVowel(String word) {
 		int vowelIndex= -1;
-		List<String> vowels = new ArrayList<String>();
-		vowels.add("a");
-		vowels.add("e");
-		vowels.add("i");
-		vowels.add("o");
-		vowels.add("u");
-		vowels.add("A");
-		vowels.add("E");
-		vowels.add("I");
-		vowels.add("O");
-		vowels.add("U");
+		int initialIndex = 100; //this value allows us to check words with multiple vowels, since no word in english will have 100 consanants before a vowel shows up
 		for(String vowel:vowels) {
-			int initialIndex = 100; //this value allows us to check words with multiple vowels, since no word in english will have 100 consanants before a vowel shows up
-			if(word.indexOf(vowel) > 0 && word.indexOf(vowel) < initialIndex) {
+				if(word.indexOf(vowel) >= 0 && word.indexOf(vowel) < initialIndex) {
+				initialIndex =  word.indexOf(vowel);
 				vowelIndex =  word.indexOf(vowel);
 			}
 		}
 		return vowelIndex;
 	}
-
-	public String[] scrapeWords(String sentance) {
-		String[] wordsArray = sentance.split(" ");
-		return wordsArray;
-	}
-
-	public String findAndMovePunctuation(String word) {
-		int punctuationIndex = word.indexOf("[.,;:?!]");
-		if(punctuationIndex >= 0) {
-			char punctuation = word.charAt(punctuationIndex);
-			word.replaceAll("[.,;:?!]", "");
-			word += punctuation;
-		}
-		return word;
-	}
-
 }
  
--- PigLatinJava/englishToPigTest/TranslatorTest.java
+++ PigLatinJava/englishToPigTest/TranslatorTest.java
@@ -11,42 +11,30 @@ public class TranslatorTest {
 	@Test
 	public void translateSentanceTest() {
 		Translator translator = new Translator();
-		String result = translator.translateSentence("This is a test Sentance.");
-		assertEquals("Is-thay is-yay a-yay est-tay Entance-say.", result);
+		String result1 = translator.translateSentence("This is a test Sentence.");
+		String result2 = translator.translateSentence("And now, for my first trick, I shall perform an act that has never been seen before!  Witness, Terry's Turnaround!");
+		assertEquals("Is-thay is-yay a-yay est-tay Entence-say.", result1);
+		assertEquals("And-yay ow-nay, or-fay y-may irst-fay ick-tray, I-yay all-shay erform-pay an-yay act-yay at-thay as-hay ever-nay een-bay een-say efore-bay!  Itness-way, Erry's-tay Urnaround-tay!", result2);
 	}
 	
 	@Test
 	public void translateWordTest() {
 		Translator translator = new Translator();
-		String result = translator.translateWord("test");
-		assertEquals("est-tay", result);
-	}
-	
-	@Test
-	public void translateWord2Test() {
-		Translator translator = new Translator();
-		String result = translator.translateWord("strength");
-		assertEquals("ength-stray", result);
+		String result1 = translator.translateWord("test");
+		String result2 = translator.translateWord("strength");
+		assertEquals("est-tay", result1);
+		assertEquals("ength-stray", result2);
 	}
 	
 	@Test
 	public void findFirstVowelTest() {
 		Translator translator = new Translator();
-		int result = translator.findFirstVowel("Strength");
-		assertEquals(3, result);
-	}
-	@Test
-	public void findFirstVowelOfNonWordTest() {
-		Translator translator = new Translator();
-		int result = translator.findFirstVowel("Strngth");
-		assertEquals(-1, result);
-	}
-	
-	@Test
-	public void findAndMovePunctuation() {
-		Translator translator = new Translator();
-		String result = translator.findAndMovePunctuation("est.-tay");
-		assertEquals("est-tay.", result);
+		int result1 = translator.findFirstVowel("Strength");
+		int result2 = translator.findFirstVowel("are");
+		int result3 = translator.findFirstVowel("Strngth");
+		assertEquals(3, result1);
+		assertEquals(0, result2);
+		assertEquals(-1, result3);
 	}
 
 }
